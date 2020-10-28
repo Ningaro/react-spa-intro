93abb54cf11e0b49968656bc80bfc49620ec19c4
Ningaro <23503775+Ningaro@users.noreply.github.com>
2020-10-28 19:40:11 +0300

    Updates

---
+++ precache-manifest.d174a5fae3a8d2398f3c7dc2cb314257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9682565fc99a88a28a1ba4a1cd731499",
+    "url": "/react-spa-intro/index.html"
+  },
+  {
+    "revision": "7378e7c48bf6e718d8f7",
+    "url": "/react-spa-intro/static/css/main.a7b80692.chunk.css"
+  },
+  {
+    "revision": "424261c54507f57c2c01",
+    "url": "/react-spa-intro/static/js/2.b92cced4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-spa-intro/static/js/2.b92cced4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7378e7c48bf6e718d8f7",
+    "url": "/react-spa-intro/static/js/main.c410c89b.chunk.js"
+  },
+  {
+    "revision": "5ec912456ae14c9cf7aa",
+    "url": "/react-spa-intro/static/js/runtime-main.6c382ab5.js"
+  }
+]);
